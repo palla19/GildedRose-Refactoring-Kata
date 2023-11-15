a02627ec0574724e06cb6149169216e9f465d748
Praneeth Alla <palla@yext.com>
2023-11-15 12:11:57 -0500

    refactored

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,58 +1,63 @@
 package gildedrose
 
+import (
+	"strings"
+)
+
 type Item struct {
 	Name            string
 	SellIn, Quality int
 }
 
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
-			}
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
-			}
+	for _, item := range items {
+		if item.Name == "Sulfuras, Hand of Ragnaros" {
+			continue
 		}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
+		item.SellIn = item.SellIn - 1
+		item.Quality = item.Quality + getDelta(item)
+		if item.Quality > 50 {
+			item.Quality = 50
+		} else if item.Quality < 0 {
+			item.Quality = 0
 		}
+	}
+}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
-					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
-				}
-			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
-			}
+func getDelta(item *Item) int {
+	isConjured, strippedName := isConjured(item)
+
+	absDelta := 1
+	if item.SellIn < 0 {
+		absDelta = absDelta * 2
+	}
+	if isConjured {
+		absDelta = absDelta * 2
+	}
+
+	isDegrading := true
+	switch strippedName {
+	case "Aged Brie":
+		isDegrading = false
+	case "Backstage passes to a TAFKAL80ETC concert":
+		isDegrading = false
+		if item.SellIn < 0 {
+			isDegrading = true
+			absDelta = item.Quality
+		} else if item.SellIn < 5 {
+			absDelta = absDelta * 3
+		} else if item.SellIn < 10 {
+			absDelta = absDelta * 2
 		}
 	}
 
+	if isDegrading {
+		return -1 * absDelta
+	}
+	return absDelta
+}
+
+func isConjured(item *Item) (bool, string){
+	return strings.HasPrefix(item.Name, "Conjured"), strings.TrimPrefix(item.Name, "Conjured ")
 }
--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -6,14 +6,108 @@ import (
 	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
 )
 
-func Test_Foo(t *testing.T) {
+func Test_AgedBrie(t *testing.T) {
 	var items = []*gildedrose.Item{
-		{"foo", 0, 0},
+		{"Aged Brie", 0, 0},
+		{"Aged Brie", -1, 0},
+		{"Aged Brie", 1, 0},
+		{"Aged Brie", -1, 49},
+		{"Aged Brie", 1, 50},
+	}
+
+	var expected = []*gildedrose.Item{
+		{"Aged Brie", -1, 2},
+		{"Aged Brie", -2, 2},
+		{"Aged Brie", 0, 1},
+		{"Aged Brie", -2, 50},
+		{"Aged Brie", 0, 50},
 	}
 
 	gildedrose.UpdateQuality(items)
 
-	if items[0].Name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+	for i, item := range items {
+		if *item != *expected[i] {
+			t.Errorf("Expected %v but got %v (test index %d)", expected[i], item, i)
+		}
+	}
+}
+
+func Test_BackstagePasses(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 10},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 10},
+		{"Backstage passes to a TAFKAL80ETC concert", 11, 10},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 48},
+		{"Backstage passes to a TAFKAL80ETC concert", 11, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", 0, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", -1, 0},
+	}
+
+	var expected = []*gildedrose.Item{
+		{"Backstage passes to a TAFKAL80ETC concert", 9, 12},
+		{"Backstage passes to a TAFKAL80ETC concert", 4, 13},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 11},
+		{"Backstage passes to a TAFKAL80ETC concert", 9, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", 4, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", -1, 0},
+		{"Backstage passes to a TAFKAL80ETC concert", -2, 0},
+	}
+
+	gildedrose.UpdateQuality(items)
+
+	for i, item := range items {
+		if *item != *expected[i] {
+			t.Errorf("Expected %v but got %v (test index %d)", expected[i], item, i)
+		}
+	}
+}
+
+func Test_Sulfuras(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Sulfuras, Hand of Ragnaros", 1, 80},
+	}
+
+	var expected = []*gildedrose.Item{
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Sulfuras, Hand of Ragnaros", 1, 80},
+	}
+
+	gildedrose.UpdateQuality(items)
+
+	for i, item := range items {
+		if *item != *expected[i] {
+			t.Errorf("Expected %v but got %v (test index %d)", expected[i], item, i)
+		}
+	}
+}
+
+func Test_RandomItem(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"Item 1", 0, 50},
+		{"Item 2", -1, 50},
+		{"Item 3", -1, 1},
+		{"Item 4", 1, 0},
+		{"Item 5", 1, 50},
+	}
+
+	var expected = []*gildedrose.Item{
+		{"Item 1", -1, 48},
+		{"Item 2", -2, 48},
+		{"Item 3", -2, 0},
+		{"Item 4", 0, 0},
+		{"Item 5", 0, 49},
+	}
+
+	gildedrose.UpdateQuality(items)
+
+	for i, item := range items {
+		if *item != *expected[i] {
+			t.Errorf("Expected %v but got %v (test index %d)", expected[i], item, i)
+		}
 	}
 }
